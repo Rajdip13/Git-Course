947081a2bbd1f8996c6df926eba50b9755a8b8d2
Rajdip Das <raj784dip@gmail.com>
2025-09-18 13:46:18 +0530

    Initial (1st) commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+        int toEven = 45;
+        sampleMethodIsEven(toEven);
+    }
+
+    static void sampleMethodIsEven(int num) {
+        String[] arr = {"even", "odd"};
+        System.out.println(arr[num%2]);
+    }
+
+}
